0cb67d111151950ce6d8ab696fc5812eec1892cd
Thiago Zanetti <thiago.project@gmail.com>
2017-08-15 15:18:05 -0300

    Adding new integration tests

---
+++ src/it/fetches-defaults/verify.groovy
@@ -0,0 +1,7 @@
+File msg_en = new File(basedir, "target/messages_en.properties");
+File msg_pt = new File(basedir, "target/messages_pt.properties");
+File msg_es = new File(basedir, "target/messages_es.properties");
+
+assert msg_en.isFile()
+assert msg_pt.isFile()
+assert msg_es.isFile()
